39c8e28287e0681b9db2a7a3661ae3718cc4a142
Tair Saetov <tsaetov@tapclap.com>
2023-03-10 01:07:41 +0500

    fixes

--- run.bat
+++ run.bat
@@ -6,15 +6,15 @@ echo %val%
 git fetch origin^
   & git fetch teach^
   & git fetch lategame^
-  & rd /s /q "./3dmigoto/Mods"^
-  & if not exist "./3dmigoto/Mods" mkdir "./3dmigoto/Mods"^
-  & Xcopy "./models" "./3dmigoto/Mods" /s^
-  & cd ./3dmigoto^
   & git add .^
   & git commit -m "yep.."^
   & git merge teach/master^
   & git merge lategame/master^
   & git push^
+  & rd /s /q "./3dmigoto/Mods"^
+  & if not exist "./3dmigoto/Mods" mkdir "./3dmigoto/Mods"^
+  & Xcopy "./models" "./3dmigoto/Mods" /s^
+  & cd ./3dmigoto^
   & start "" "./3DMigoto Loader.exe"
 timeout /t 5
 start "" "%val%"
