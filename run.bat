ded4861226d2475b4013633971a8f41e59c70279
Tair Saetov <tsaetov@tapclap.com>
2023-03-09 22:47:33 +0500

    fix commitment

--- run.bat
+++ run.bat
@@ -11,7 +11,7 @@ git fetch origin^
   & git checkout teach^
   & git checkout pull^
   & git add .^
-  & git commit^
+  & git commit "yep.."^
   & git merge origin/master^
   & git merge origin/sniper^
   & git push^
