599d18ae54fcc3a0578911f7aaf46677e55f204b
Tair Saetov <tsaetov@tapclap.com>
2023-03-09 22:56:34 +0500

    some changes in git logic

--- run.bat
+++ run.bat
@@ -8,14 +8,11 @@ git fetch origin^
   & if not exist "./3dmigoto/Mods" mkdir "./3dmigoto/Mods"^
   & Xcopy "./models" "./3dmigoto/Mods" /s^
   & cd ./3dmigoto^
-  & git checkout teach^
-  & git checkout pull^
   & git add .^
   & git commit -m "yep.."^
   & git merge origin/master^
   & git merge origin/sniper^
   & git push^
-  & git checkout master^
   & start "" "./3DMigoto Loader.exe"
 timeout /t 5
 start "" "%val%"
